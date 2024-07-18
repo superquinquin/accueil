040114a0075a7dedac965a374bab5caed96b684a
morague <rom88.viry@gmail.com>
2024-07-19 01:23:56 +0200

    refactor(static): refactor css & js for shifts templates. addind error handling and notifications

--- accueil/static/js/shifts.js
+++ accueil/static/js/shifts.js
@@ -8,6 +8,10 @@ websocket.addEventListener("message", (event) => {
 
     // event shift unrelated & without data. implies an early return.
     switch (payload.message) {
+        case "error":
+            closeRegistrationModal();
+            new MsgFactory("msg-box", "err", "<strong>" + payload.data.reasons + "</strong>", true, 3000, 1000);
+            break;
         case "reload":
             window.location.reload();
             return;
@@ -28,7 +32,7 @@ websocket.addEventListener("message", (event) => {
             member.setAttribute('onclick', "askConfirmationReset("+data.registration_id+")");
             member.setAttribute("state", "done");
             member.className = "";
-            member.classList.add("done");
+            member.classList.add("done", "border-light");
             member.innerHTML += "<span>✓</span>";
             break;
         case "reset":
@@ -44,7 +48,7 @@ websocket.addEventListener("message", (event) => {
     
             member.setAttribute("state", "open");
             member.className = "";
-            member.classList.add("open");
+            member.classList.add("open", "border-light");
             break;
         case "closeCmodal":
             CloseCModal();
@@ -53,12 +57,13 @@ websocket.addEventListener("message", (event) => {
             console.log(data);
             let shift = document.getElementById(data.shift_id.toString());
             let container = shift.getElementsByTagName("ul")[0];
+            if (container.getElementsByClassName("placeholder").length > 0) {
+                container.innerHTML = "";
+            }
             container.innerHTML += data.html;
             break;
-        case "CloseCatchingUpModal":
-            CloseCatchingUpModal();
-            break;
-        case "error":
+        case "closeRegistrationModal":
+            closeRegistrationModal();
             break;
         default:
             console.log("unknown case.");
@@ -72,7 +77,14 @@ function askConfirmation(registration_id) {
     let partner_id = parseInt(member.getAttribute('partner_id'));
     let shift_id = parseInt(member.getAttribute('shift_id'));
 
-    let body = "Bonjour "+member.innerHTML.split('-')[1]+ "\nVeuillez confirmer votre présence";
+    let name = member.getAttribute("name");
+    let associate_name = member.getAttribute("associate_name");
+    let display_name = name;
+    if (associate_name != "None") {
+        display_name = name + " et/ou " + associate_name;
+    }
+
+    let body = "Bonjour <strong>" + display_name + "</strong><br>Veuillez confirmer votre présence";
     let payload = registration_id+", "+partner_id+", "+shift_id;
     openCModal(
         "Confirmation de présence",
@@ -83,6 +95,8 @@ function askConfirmation(registration_id) {
 
 async function confirmedPresence(registration_id, partner_id, shift_id) {
     websocket.send(JSON.stringify({"message": "attend", data: {"registration_id": registration_id, "partner_id": partner_id, "shift_id": shift_id}}));
+    document.getElementById('cancel-confirmation').desabled = true;
+    document.getElementById('accept-confirmation').desabled = true;
 }
 
 // NOTING RESET ATTENDANCIES
@@ -91,7 +105,14 @@ function askConfirmationReset(registration_id) {
     let partner_id = parseInt(member.getAttribute('partner_id'));
     let shift_id = parseInt(member.getAttribute('shift_id'));
 
-    let body = "Bonjour, Voullez vous annuler la présence de "+member.innerHTML.split('-')[1].split("✓")[0];
+    let name = member.getAttribute("name");
+    let associate_name = member.getAttribute("associate_name");
+    let display_name = name;
+    if (associate_name != "None") {
+        display_name = name + " et/ou " + associate_name;
+    }
+
+    let body = "Bonjour, Voullez vous annuler la présence de <strong>"+display_name+"</strong>";
     let payload = registration_id+", "+partner_id+", "+shift_id;
     openCModal(
         "Annuler sa présence",
@@ -102,47 +123,25 @@ function askConfirmationReset(registration_id) {
 
 async function confirmedReset(registration_id, partner_id, shift_id) {
     websocket.send(JSON.stringify({"message": "reset", data: {"registration_id": registration_id, "partner_id": partner_id, "shift_id": shift_id}}));
+    document.getElementById('cancel-confirmation').disabled = true;
+    document.getElementById('accept-confirmation').disabled = true;
 }
-
-
-
-function rescaleCModalContainer(action) {
-    let ch = document.getElementById('confirmation-container').clientHeight;
-    let cw = document.getElementById('confirmation-container').clientWidth;
-    let h = document.getElementById('confirmation-container').style.height;
-    let w = document.getElementById('confirmation-container').style.width;
-    // apply rescaling for computer screen on error modal
-    if (action == "open" && ch == 250 && cw == 300) {
-      document.getElementById('confirmation-container').style.height = "80%";
-      document.getElementById('confirmation-container').style.width = "50%";
-    } else if (action == "open" && h == "80%" && w == "50%") {
-      document.getElementById('confirmation-container').style.height = "250px";
-      document.getElementById('confirmation-container').style.width = "300px";
-    } else if (action == "close") {
-      document.getElementById('confirmation-container').style.height = "250px";
-      document.getElementById('confirmation-container').style.width = "300px";
-    }
-}
-
 
 function openCModal(headerMsg, bodyMsg, closeFunc) {
-    console.log('modal open')
     window.scrollTo(0, window.scrollY); 
-    document.getElementById('confirmation-hub-modal').style.top = (window.scrollY - 5).toString() + 'px';
-    document.getElementById('confirmation-hub-modal').style.display = 'flex';
+    document.getElementById('confirmation-modal').style.top = (window.scrollY - 5).toString() + 'px';
+    document.getElementById('confirmation-modal').style.display = 'flex';
     document.getElementById('html').style.overflowY = 'hidden';
     document.getElementById('heading-message').innerHTML = headerMsg;
     document.getElementById('content-message').innerHTML = bodyMsg;
-    // rescaleCModalContainer()
     document.getElementById('accept-confirmation').setAttribute('onclick', closeFunc);
 }
 
 function CloseCModal() {
-    document.getElementById('confirmation-hub-modal').style.display = 'none';
+    document.getElementById('confirmation-modal').style.display = 'none';
     document.getElementById('html').style.overflowY = 'visible';
-    document.getElementById('cancel-confirmation').hidden = false;
-    document.getElementById('accept-confirmation').hidden = false;
-    // rescaleCModalContainer("close")
+    document.getElementById('cancel-confirmation').disabled = false;
+    document.getElementById('accept-confirmation').disabled = false;
 }
 
 function referenced_shifts_ids() {
@@ -155,33 +154,37 @@ function referenced_shifts_ids() {
 }
 
 
-function askCatchingUp(shift_id) {
-    document.getElementById("member-list").classList.add(shift_id);
-    OpenCatchingUpModal()
+
+
+// REGISTRATION
+function askRegistration(shift_id) {
+    document.getElementById("member-list").setAttribute("shift_id", shift_id);
+    openRegistrationModal()
 }
 
-function OpenCatchingUpModal() {
-    console.log('modal open')
+function openRegistrationModal() {
     window.scrollTo(0, window.scrollY); 
-    document.getElementById('catchingUp-modal').style.top = (window.scrollY - 5).toString() + 'px';
-    document.getElementById('catchingUp-modal').style.display = 'flex';
+    document.getElementById('registration-modal').style.top = (window.scrollY - 5).toString() + 'px';
+    document.getElementById('registration-modal').style.display = 'flex';
     document.getElementById('html').style.overflowY = 'hidden';
 }
 
-function CloseCatchingUpModal() {
-    let shift_id = document.getElementById("member-list").classList[0]
-    document.getElementById('catchingUp-modal').style.display = 'none';
+function closeRegistrationModal() {
+    document.getElementById('registration-modal').style.display = 'none';
     document.getElementById('html').style.overflowY = 'visible';
     document.getElementById("member-list").innerHTML = "";
-    document.getElementById("member-list").classList.remove(shift_id);
-    document.getElementById("search-CatchinUp").value = "";
+    document.getElementById("member-list").removeAttribute("shift_id");
+    document.getElementById("search-member-input").value = "";
 }
 
-async function searchMember(id) {
-    let loading = document.getElementById("loading");
+async function searchMember() {
+    let loading = document.getElementById("loader");
+    let container = document.getElementById("member-list");
+    container.innerHTML = "";
+    container.style.display = "none";
     loading.style.display = "block";
 
-    let value = document.getElementById(id).value;
+    let value = document.getElementById("search-member-input").value;
     payload = {
         method: "POST", 
         mode: "cors", 
@@ -195,45 +198,64 @@ async function searchMember(id) {
     const response = await fetch("./registration/search_member", payload);
     const data = await response.json();
 
-    loading.style.display = "none";
-
     if (response.status != 200) {
-        console.log(data.data);
-
+        new MsgFactory("msg-box", "err", "<strong>" + data.reasons + "</strong>", true, 3000, 1000);
     } else {
-        console.log(data.data);
         if (data.data.length > 0) {
-            let container = document.getElementById("member-list");
-            let ul = document.createElement("ul");
-            for (var member of data.data) {
-                let display_name = member.barcode_base+" - "+member.name
-                let elm = '<li class="li-member" partner_id="'+member.partner_id+'" barcode="'+member.barcode_base+'"><input type="checkbox" onclick="OnlyOneCheckRemain(this)"><span>'+display_name+'</span></li>';
-                ul.innerHTML += elm;
-            }
+            let ul = searchMemberListFactory(data.data);
             container.appendChild(ul);
         } else {
             let container = document.getElementById("member-list");
-            let ul = document.createElement("ul");
-            let elm = "<li>Aucun résultat</li>"
-            ul.innerHTML += elm;
-            container.appendChild(ul);
+            let elm = "<ul><li>Aucun résultat</li></ul>"
+            container.innerHTML += elm;
         }
     }
 
+    loading.style.display = "none";
+    container.style.display = "block";
 }
 
-function registerCatchUp() {
+
+function searchMemberListFactory(payload) {
+    let ul = document.createElement("ul");
+    ul.classList.add("border");
+    for (var member of payload) {
+        let elm = searchMemberFactory(member);
+        ul.appendChild(elm);
+    }
+    return ul;
+}
+
+function searchMemberFactory(data) {
+    let display_name = "<strong>" + data.barcode_base+"</strong> - "+data.name
+
+    let box = document.createElement("input");
+    box.setAttribute("type", "checkbox");
+    box.setAttribute("onclick", "OnlyOneCheckRemain(this)")
+    box.setAttribute("hidden", true);
+
+    let label = document.createElement("label");
+    label.classList.add("li-member","border-light"); // 
+    label.setAttribute("partner_id", data.partner_id);
+    label.setAttribute("barcode", data.barcode_base);
+    label.innerHTML = display_name;
+
+    label.appendChild(box);
+    return label;
+}
+
+function registrateMember() {
     let container = document.getElementById('member-list');
     for (li of container.getElementsByClassName("li-member")) {
-        if (li.firstChild.checked) {
+        if (li.lastChild.checked) {
             let context = {
-                "shift_id": parseInt(container.classList[0]),
+                "shift_id": parseInt(container.getAttribute("shift_id")),
                 "partner_id": parseInt(li.getAttribute("partner_id")),
             }
+            console.log(context);
             websocket.send(JSON.stringify({"message": "registrate", "data": context}));
         }
     }
-    // CloseCatchingUpModal()
 }
 
 function OnlyOneCheckRemain(elm) {
@@ -242,7 +264,81 @@ function OnlyOneCheckRemain(elm) {
     for (li of ul.getElementsByClassName('li-member')) {
         let other_id = li.getAttribute("partner_id");
         if (id != other_id) {
-            li.firstChild.checked = false;
+            li.lastChild.checked = false;
+            li.classList.remove("selected");
+        } else {
+            li.classList.add("selected");
+        }
+
+    }
+}
+
+// NOTIFICATIONS
+
+class MsgFactory {
+    constructor(containerId, mtype, msg, fade=false, sleep=0, speed=0) {
+        const container = document.getElementById(containerId);
+
+        let frame = this.buildFrame(mtype);
+        frame.appendChild(this.buildSymbol(mtype));
+        frame.appendChild(this.buildMsg(msg));
+        frame.appendChild(this.buildExit());
+
+        let node = container.appendChild(frame);
+        if (fade) {
+            this.fadeOut(node, sleep, speed);
         }
     }
+
+
+    buildFrame(mtype) {
+        let frame = document.createElement("div");
+        frame.classList.add(mtype, "msg-container");
+        frame.style.opacity = 100;
+        return frame;
+    }
+
+    buildSymbol(mtype) {
+        let elm = document.createElement('img');
+        elm.classList.add("msg-symbol","msg-comp"); 
+        elm.setAttribute("src","/static/misc/"+mtype+".png");
+        return elm;
+    }
+
+    buildMsg(msg) {
+        let elm = document.createElement("p");
+        elm.classList.add("msg", "msg-comp");
+        elm.innerHTML = msg;
+        return elm;
+    }
+
+    buildExit() {
+        let elm = document.createElement("div");
+        elm.classList.add("quit-container");
+        
+        let btn = document.createElement("button");
+        btn.classList.add("msg-quit", "msg-comp");
+        btn.setAttribute("onclick", "closeMsg(this)");
+        btn.innerText = "x";
+
+        elm.appendChild(btn);
+        return elm;   
+    }
+
+    async fadeOut(node, slp, speed) {
+        await sleep(slp);
+        node.style.transition = speed+'ms';
+        node.style.opacity = 0;
+        await sleep(speed);
+        node.remove();
+    }
+}
+
+function closeMsg(elm) {
+    let box = elm.parentElement.parentElement;
+    box.remove();
+}
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
